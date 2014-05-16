0280f362a0ef0703c4b2c64cea53aea72ec3db24
Freika <iamfrey@gmail.com>
2014-05-16 17:30:08 +0700

    Fixed: samp backgrounds

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,4 +1,8 @@
-class StatisticsController < InheritedResources::Base
+class StatisticsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
+
+  def index
+    @statistics = Statistic.all
+  end
 
 end
