0a77d0d2232e743a7c4180fb5298699db16ddb79
Freika <iamfrey@gmail.com>
2014-05-24 21:09:54 +0700

    Changed: jquery to cdn via gem

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
-gem 'jquery-rails'
+gem 'jquery-rails-cdn', '~> 1.0.2'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Nick
   class Application < Rails::Application
+
+    config.assets.precompile += ['jquery.js']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
