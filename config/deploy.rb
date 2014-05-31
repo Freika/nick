57f6a35b0d0e233114f4663d177d337a6caf35f8
Freika <iamfrey@gmail.com>
2014-05-31 22:13:21 +0700

    Fix: deploy.rb(remove whenever scenario)

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,10 +81,6 @@ set :scm,             :git
 ## Если ваш репозиторий в GitHub, используйте такую конфигурацию
 # set :repository,    "git@github.com:username/project.git"
  set :repository,    "git@github.com:Freika/nick.git"
- 
-
- set :whenever_command, "rvm use #{rvm_ruby_string} do bundle exec whenever"
- after 'deploy:update_code','whenever:update_crontab'
 ## --- Ниже этого места ничего менять скорее всего не нужно ---
 
 before 'deploy:finalize_update', 'set_current_release'
