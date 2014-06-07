e4d9225c64ccc1c806e0282537a4b00761d6941e
Freika <iamfrey@gmail.com>
2014-06-07 16:17:27 +0700

    Added: nick graph(dont work)

---
+++ app/assets/javascripts/statistic.js.coffee
@@ -0,0 +1,8 @@
+jQuery ->
+  new Morris.Line({
+    element: 'nicks_chart',
+    data: $('#nicks_chart').data('nicks'),
+    xkey: 'created_at',
+    ykeys: ['count'],
+    labels: ['Сгенерировано']
+});
--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,2 +1,10 @@
 module StatisticsHelper
+  def statistics_chart_data
+    (1.month.ago.to_date..Date.today).map do |date|
+      {
+        created_at: date,
+        count: Statistic.where("date(created_at) = ?", date).count
+      }
+    end
+  end
 end
