f4fd520e15a0a3ee38116ba3154921822e6c02d9
Freika <iamfrey@gmail.com>
2015-08-19 02:05:26 +0600

    Update seeds.

--- db/migrate/20141211224656_change_gw_human_slug.rb
+++
@@ -1,7 +0,0 @@
-class ChangeGwHumanSlug < ActiveRecord::Migration
-  def change
-    human = Race.where(game_id: Game.find_by(slug: 'gw2').id, slug: 'human').first
-    human.slug = 'human-of-tyria'
-    human.save
-  end
-end
