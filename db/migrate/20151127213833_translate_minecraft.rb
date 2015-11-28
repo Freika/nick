faa763dedde16d2bf676c20d93934ba57354d59b
Freika <iamfrey@gmail.com>
2015-11-28 23:07:32 +0600

    Update seeds.

--- db/migrate/20151127213833_translate_minecraft.rb
+++ db/migrate/20151127213833_translate_minecraft.rb
@@ -6,10 +6,5 @@ class TranslateMinecraft < ActiveRecord::Migration
     man.title = 'Minecraft nicknames generator, nicknames for minecraft by skins'
     man.content = 'Nicknames for Minecraft can be any. Nevertheless, many servers demand that the name corresponded to the atmosphere of game. The player has to create a role nickname or a name for the character of Minecraft. On this page you can create a set of nicknames for boys. Simply press the button above and choose that is pleasant to you.'
     man.save!
-
-    girl = Race.find_by(slug: 'woman')
-    girl.name = ''
-    girl.title = ''
-    girl.content = ''
   end
 end
