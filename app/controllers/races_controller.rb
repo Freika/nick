8a2a817ba756b337c7ee8d86b13d74bdfc159755
Freika <iamfrey@gmail.com>
2016-07-23 12:30:20 +0700

    Remove unnecessary gems and races files.

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -9,35 +9,6 @@ class RacesController < ApplicationController
     @game = @race.game
   end
 
-  def new
-    @race = Race.new
-  end
-
-  def edit
-  end
-
-  def create
-    @race = Race.new(race_params)
-    if @race.save
-      redirect_to @race, notice: 'Раса создана'
-    else
-      render action: 'new'
-    end
-  end
-
-  def update
-    if @race.update(race_params)
-      redirect_to @race, notice: 'Раса обновлена'
-    else
-      render action: 'edit'
-    end
-  end
-
-  def destroy
-    @race.destroy
-    redirect_to races_url
-  end
-
   private
 
   def set_race
