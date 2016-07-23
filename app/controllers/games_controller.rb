83fb1bf44efa9f0b61141b0fb68ee8b71a12ffd6
Freika <iamfrey@gmail.com>
2016-07-23 12:28:08 +0700

    Remove unnecessary files.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,40 +1,11 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :edit, :update, :destroy]
+  before_action :set_game, only: :show
 
   def index
     @game = Game.where(slug: 'wow').first
   end
 
   def show
-  end
-
-  def new
-    @game = Game.new
-  end
-
-  def edit
-  end
-
-  def create
-    @game = Game.new(game_params)
-    if @game.save
-      redirect_to @game, notice: 'Игра создана'
-    else
-      render action: 'new'
-    end
-  end
-
-  def update
-    if @game.update(game_params)
-      redirect_to @game, notice: 'Игра обновлена'
-    else
-      render action: 'edit'
-    end
-  end
-
-  def destroy
-    @game.destroy
-    redirect_to games_url
   end
 
   private
