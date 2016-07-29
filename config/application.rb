a7e2b7dc4432bf1f4987cb6a04e0040775329384
Freika <iamfrey@gmail.com>
2016-07-29 20:57:46 +0700

    Update config.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ module Nick
   class Application < Rails::Application
     config.assets.precompile += ['jquery.js']
     config.cache_expires_in = 24*60*60
-    config.force_ssl = true if Rails.env.production?
+    # config.force_ssl = true if Rails.env.production?
     config.i18n.available_locales = [:en, :ru]
     config.i18n.default_locale = :ru
   end
