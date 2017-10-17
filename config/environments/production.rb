6dfcbaf3cca4818199e01f33260494ca52245ac4
Eugene Burmakin <iamfrey@gmail.com>
2017-10-18 01:30:52 +0700

    Use brand new settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.serve_static_files = true
+  config.public_file_server.enabled = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
