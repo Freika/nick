3b494f9d6bd0ad7434b5477dbee75c35caaa61e9
Eugene Burmakin <iamfrey@gmail.com>
2020-04-19 22:37:11 +0300

    Updated race page.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,8 @@ var Turbolinks = require("turbolinks")
 Turbolinks.start()
 require("jquery")
 
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
 import 'bootstrap'
 import '../src/application.scss'
