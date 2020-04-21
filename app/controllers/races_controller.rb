208a480e3db97add739dd103444e6e4c1c4210a7
Eugene Burmakin <iamfrey@gmail.com>
2020-04-21 19:22:13 +0300

    Fix generating names for all races.

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -3,6 +3,5 @@ class RacesController < ApplicationController
 
   def show
     @race = Race.includes(:game).find_by(slug: params[:id])
-    @game = @race.game
   end
 end
--- app/reflexes/nick_reflex.rb
+++ app/reflexes/nick_reflex.rb
@@ -2,6 +2,21 @@
 
 class NickReflex < ApplicationReflex
   def generate
-    @nick = Nickname.generate_wow(element.dataset[:race], element.dataset[:sex])[:name]
+    race = element.dataset[:race]
+    sex = element.dataset[:sex]
+
+    @nick =
+      if Nickname::WOW_RACES.include?(race)
+        Nickname.generate_wow(race, sex)
+      elsif race == 'asura'
+        Nickname.generate_gw_asura(race, sex)
+      elsif race == 'sylvari'
+        Nickname.generate_gw_sylvari(race, sex)
+      elsif Nickname::GW_RACES.include?(race)
+        Nickname.generate_gw(race, sex)
+      elsif Nickname::SAMP_RACES.include?(race)
+        Nickname.generate_samp(race, sex)
+      end[:name]
+    # @nick = Nickname.generate_wow(race, sex)[:name]
   end
 end
