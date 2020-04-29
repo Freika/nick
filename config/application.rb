b1bf6d1e05685ac97e35933b9dbcea590dd8960a
Eugene Burmakin <iamfrey@gmail.com>
2020-04-29 19:31:25 +0300

    Remove unnecessary files.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(*Rails.groups)
 
 module Nick
   class Application < Rails::Application
-    config.load_defaults 5.0
+    config.load_defaults 6.0
 
     config.i18n.available_locales = [:en, :ru]
     config.i18n.default_locale = :ru
